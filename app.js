2516f26796fca8fd77406ee232856f6233099051
igorashs <igorashsirbu@gmail.com>
2019-08-31 20:52:08 +0300

    Add Book function constructor

---
+++ app.js
@@ -0,0 +1,7 @@
+function Book(title, author, totalPage, completedPages, completed) {
+  this.title = title;
+  this.author = author;
+  this.totalPage = totalPage;
+  this.completedPages = completedPages;
+  this.completed = completed;
+}
